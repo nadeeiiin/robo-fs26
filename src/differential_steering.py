a29d68963ab53c3dbbe97ad450eff99d3de60fdb
nadine <nadine_flueck@hotmail.com>
2026-03-22 17:14:25 +0000

    aufgabe 2, 3

--- src/differential_steering.py
+++ src/differential_steering.py
@@ -3,9 +3,14 @@
 import rospy
 import tf
 import os
+import math
 
 from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped
 from tf.transformations import quaternion_from_euler
+
+WHEEL_RADIUS = 0.0335
+WHEELBASE = 0.1
+TICKS_RESOLUTION = 144
 
 class DifferentialSteering:
 
@@ -18,27 +23,72 @@ class DifferentialSteering:
         self.wheel_command_publisher = rospy.Publisher(wheel_command_topic, WheelsCmdStamped, queue_size = 10)
         
         # subscribe wheel tick messages
-        left_wheel_tick_topic = '/' + robot_name + '/left_wheel_encoder_node/tick'
-        right_wheel_tick_topic = '/' + robot_name + '/right_wheel_encoder_node/tick'
+        left_wheel_tick_topic = '/' + robot_name + '/left_wheel_encoder_driver_node/tick'
+        right_wheel_tick_topic = '/' + robot_name + '/right_wheel_encoder_driver_node/tick'
         rospy.Subscriber(left_wheel_tick_topic, WheelEncoderStamped, self.callback_left_wheel_tick)
         rospy.Subscriber(right_wheel_tick_topic, WheelEncoderStamped, self.callback_right_wheel_tick)
 
         self.transform_broadcaster = tf.TransformBroadcaster()
+
+        # internal pose state (to track pose in memory)
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+
+        # tick storage
+        self.left_ticks = None
+        self.right_ticks = None
 
         # set the robot at pose (0,0,0)
         self.publish_transform(x=0,y=0,theta=0)
        
 
     def callback_left_wheel_tick(self, data):
-        left_tick_count = data.data
+        self.left_ticks = data.data
     
     def callback_right_wheel_tick(self, data):
-        right_tick_count = data.data
+        self.right_ticks = data.data
+
+    def update_pose(self, delta_left, delta_right):
+        # convert number of ticks to real distance
+        # distance = (ticks/ticks per res) * circumference
+        d_left = (delta_left / TICKS_RESOLUTION) * 2 * math.pi * WHEEL_RADIUS
+        d_right = (delta_right / TICKS_RESOLUTION) * 2 * math.pi * WHEEL_RADIUS
+
+        # apply differential drive formulas
+        V = (d_right + d_left) / 2
+        w = (d_right - d_left) / WHEELBASE
+
+        self.x += V * math.cos(self.theta)
+        self.y += V * math.sin(self.theta)
+        self.theta += w
 
     def run(self):
+            while self.left_ticks is None or self.right_ticks is None:
+                rospy.sleep(0.05)
+
+            # store starting tick value
+            prev_left = self.left_ticks
+            prev_right = self.right_ticks
+
+            rate = rospy.Rate(20)
             while not rospy.is_shutdown():
-                pass
+                curr_left = self.left_ticks
+                curr_right = self.right_ticks
+
+                delta_left = curr_left - prev_left
+                delta_right = curr_right - prev_right
+
+                if delta_left != 0 or delta_right != 0:
+                    self.update_pose(delta_left, delta_right)
+                    self.publish_transform(self.x, self.y, self.theta)
+                    rospy.loginfo(f"x={self.x:.3f} y={self.y:.3f} θ={math.degrees(self.theta):.1f} degrees")
         
+                prev_left = curr_left
+                prev_right = curr_right
+
+                rate.sleep()
+
     def publish_transform(self, x, y, theta):
         """publishes a transform between the map frame and the robot_name/base frame
                 Parameters:
@@ -52,6 +102,6 @@ class DifferentialSteering:
 
 
 if __name__ == '__main__':
-    vehicle_name = os.getenv('VEHICLE_NAME', 'alpha')
+    vehicle_name = os.getenv('VEHICLE_NAME', 'pi')
     ds = DifferentialSteering(vehicle_name)
     ds.run()
---
+++ src/drive_to_goal.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import rospy
+import os
+import math
+from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped
+from std_msgs.msg import Header
+
+WHEEL_RADIUS = 0.0335
+WHEELBASE = 0.1
+TICKS_RESOLUTION = 144
+
+class DriveToGoal:
+    def __init__(self, robot_name):
+        rospy.init_node('drive_to_goal')
+        self.left_ticks = None
+        self.right_ticks = None
+
+        self.pub = rospy.Publisher('/' + robot_name + '/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
+        rospy.Subscriber('/' + robot_name + '/left_wheel_encoder_driver_node/tick',  WheelEncoderStamped, self.cb_left)
+        rospy.Subscriber('/' + robot_name + '/right_wheel_encoder_driver_node/tick', WheelEncoderStamped, self.cb_right)
+        rospy.sleep(2.0)
+
+    def cb_left(self, msg):
+        self.left_ticks = msg.data
+
+    def cb_right(self, msg):
+        self.right_ticks = msg.data
+
+    def set_wheels(self, vel_left, vel_right):
+        cmd = WheelsCmdStamped()
+        cmd.header.stamp = rospy.Time.now()
+        cmd.vel_left = vel_left
+        cmd.vel_right = vel_right
+        self.pub.publish(cmd)
+
+    def drive_ticks(self, ticks, vel_l, vel_r):
+        while self.left_ticks is None or self.right_ticks is None:
+            rospy.sleep(0.05)
+        start_l, start_r = self.left_ticks, self.right_ticks
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            if (abs(self.left_ticks - start_l) + abs(self.right_ticks - start_r)) / 2 >= ticks:
+                break
+            self.set_wheels(vel_l, vel_r)
+            rate.sleep()
+        self.set_wheels(0.0, 0.0)
+        rospy.sleep(0.5)
+    
+    def to_ticks(self, distance):
+        return int(distance / (2* math.pi * WHEEL_RADIUS) * TICKS_RESOLUTION)
+
+    def rotate(self, angle):
+        ticks = self.to_ticks(abs(angle) * WHEELBASE / 2)
+        self.drive_ticks(ticks, -0.3 if angle > 0 else 0.3, 0.3 if angle > 0 else -0.3)
+
+    def run(self, goal_x, goal_y)
+    angle = math.atan2(goal_y, goal_x)
+    distance = math.sqrt(goal_x ** 2 + goal_y ** 2)
+
+    self.rotate(angle)
+    self.drive_ticks(self.to_ticks(distance), 0.3, 0.3)
+    rospy.loginfo("done!")
+
+if __name__ == '__main__':
+    robot = DriveToGoal(os.getenv('VEHICLE_NAME', 'pi'))
+    robot.run(
+        goal_x = 0,
+        goal_y = 0
+    )
